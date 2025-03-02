d3ac08719bf2f3bdb7027835a726f4bcb54de9c4
JenikAdamira <jenik.adamira@gmail.com>
2025-03-02 14:26:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const formular = document.getElementById("formular");
 const tabulka = document.getElementById("tabulka").getElementsByTagName("tbody")[0];
 const vyhledavani = document.getElementById("vyhledavani");
 
-const URL_GOOGLE_APPS_SCRIPT = "TVOJE_API_URL"; // Zde vlož svou API URL
+const URL_GOOGLE_APPS_SCRIPT = "https://script.google.com/macros/s/AKfycbyalBE7b-c6caOe51Q7lXdVOE3xS6TsCg7heJa7Sr67gy9oz53J4T21WO497UierASN/exec"; // Zde vlož svou API URL
 
 let smlouvy = JSON.parse(localStorage.getItem("smlouvy")) || [];
 
