eaf4886c19e38c5353a0639367618fc330d8d399
JenikAdamira <jenik.adamira@gmail.com>
2025-03-02 20:29:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const GOOGLE_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyD13zrD75Ri2jV6--COrlHa59my30xxx-yZeFyFmnRZPcEBB-Z8kICQXkTwGNbaurL/exec";
+const GOOGLE_APPS_SCRIPT_URL = "https://script.google.com/macros/s/TVOJE_API_URL/exec";
 
+// Zpracování POST požadavků
 app.post("/", async (req, res) => {
     try {
         const response = await fetch(GOOGLE_APPS_SCRIPT_URL, {
@@ -22,5 +23,8 @@ app.post("/", async (req, res) => {
         res.status(500).json({ error: "Chyba při odesílání na Google Apps Script" });
     }
 });
+
+// Přidání GET route pro favicon.ico (řeší chybu 404)
+app.get("/favicon.ico", (req, res) => res.status(204));
 
 module.exports = app;
